fca3cb446533b06e02626cca627b5986fdbd604c
chloepark195 <spark220@chicagobooth.edu>
2022-02-18 06:16:53 +0000

    delete works

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,7 +3,35 @@ class PhotosController < ActionController::Base
     matching_photos = Photo.all
 
     @list_of_photos = matching_photos.order({ :created_at => :desc })
-    
+
     render({ :template => "photo_templates/index.html.erb"})
   end
+  
+  def show
+
+    url_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({ :id => url_id})
+
+    @the_photo = matching_photos.at(0)
+
+    render({ :template =>"photo_templates/show.html.erb"})
+  end
+
+  def baii
+    the_id = params.fetch("toast_id")
+
+    matching_photos = Photo.where({ :id => the_id })
+
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+
+    #render({ :template => "photo_templates/baii.html.erb"})
+
+    redirect_to("/photos")
+  end
+
+
+
 end
